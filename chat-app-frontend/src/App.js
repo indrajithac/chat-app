77db7d51df8b0dd573960fc426b1b9a2a448693c
Indrajith A C <indrajithac@gmail.com>
2022-03-28 10:36:58 +0530

    socket event added

--- chat-app-frontend/src/App.js
+++ chat-app-frontend/src/App.js
@@ -6,25 +6,36 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Chat from './pages/Chat'
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { AppContext, socket } from './context/appContext';
 
 
 function App() {
+  const [rooms, setRooms] = useState([])
+  const [currentRoom, setCurrentRoom] = useState([])
+  const [members, setMembers] = useState([])
+  const [messages, setMessages] = useState([])
+  const [personalMessage, setPersonalMessage] = useState({})
+  const [newMessage, setNewMessage] = useState({})
+
   const user = useSelector((state) => state.user)
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        {!user && (
-          <>
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<Signup />} />
-          </>
-        )}
-        <Route path='/chat' element={<Chat />} />
+    <AppContext.Provider value={{ socket, rooms, setRooms, currentRoom, setCurrentRoom, members, setMembers, messages, setMessages, personalMessage, setPersonalMessage,newMessage,setNewMessage }}>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {!user && (
+            <>
+              <Route path='/login' element={<Login />} />
+              <Route path='/signup' element={<Signup />} />
+            </>
+          )}
+          <Route path='/chat' element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
 
-      </Routes>
-    </BrowserRouter>
   );
 }
 
